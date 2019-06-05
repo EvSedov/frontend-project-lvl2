bfd5b81113223510ca1406c301a5d680e529f817
Evgeniy Sedov <sedoff-online@yandex.ru>
2019-06-05 23:46:54 +0700

    added function gendiff

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -1,17 +1,11 @@
+import fs from 'fs';
 import gendiff from '../dist/gendiff';
 
 test('compares two configuration files and shows a difference', () => {
-  const beforeJson = {
-    host: 'hexlet.io',
-    timeout: 50,
-    proxy: '123.234.53.22',
-    follow: false,
-  };
-  const afterJson = {
-    timeout: 20,
-    verbose: true,
-    host: 'hexlet.io',
-  };
-  const result = '{host: hexlet.io, + timeout: 20, - timeout: 50, - proxy: 123.234.53.22, + verbose: true, - follow: false}';
-  expect(gendiff(beforeJson, afterJson)).toBe(result);
+  const pathToBefore = './__test__/__fixtures__/before.json';
+  const pathToAfter = './__test__/__fixtures__/after.json';
+  fs.openSync(pathToBefore, 'r');
+  fs.openSync(pathToAfter, 'r');
+  const result = '{\n\n   host: hexlet.io\n - timeout: 50\n + timeout: 20\n - proxy: 123.234.53.22\n - follow: false\n + verbose: true\n}';
+  expect(gendiff(pathToBefore, pathToAfter)).toBe(result);
 });
--- src/gendiff/index.js
+++ src/gendiff/index.js
@@ -1,2 +1,36 @@
-const gendiff = (configFilePathBefore, configFilePathAfter) => '{host: hexlet.io, + timeout: 20, - timeout: 50, - proxy: 123.234.53.22, + verbose: true, - follow: false}';
+import fs from 'fs';
+import path from 'path';
+import _ from 'lodash';
+
+const getPathAbsolute = pathToFile => ((path.isAbsolute(pathToFile))
+  ? pathToFile : path.normalize(`${process.cwd()}/${pathToFile}`));
+
+const gendiff = (filePathBefore, filePathAfter) => {
+  const filePathBeforeAbsolute = getPathAbsolute(filePathBefore);
+  const filePathAfterAbsolute = getPathAbsolute(filePathAfter);
+  const fdBefore = fs.openSync(filePathBeforeAbsolute, 'r');
+  const fdAfter = fs.openSync(filePathAfterAbsolute, 'r');
+  const fileContentBefore = JSON.parse(fs.readFileSync(filePathBeforeAbsolute));
+  const fileContentAfter = JSON.parse(fs.readFileSync(filePathAfterAbsolute));
+  const keys = _.uniq([..._.keys(fileContentBefore), ..._.keys(fileContentAfter)]);
+  let result = '';
+  keys.forEach((key) => {
+    if (_.has(fileContentBefore, key) && _.has(fileContentAfter, key)) {
+      if (fileContentBefore[key] === fileContentAfter[key]) {
+        result = `${result}\n   ${key}: ${fileContentBefore[key]}`;
+      } else {
+        result = `${result}\n - ${key}: ${fileContentBefore[key]}\n + ${key}: ${fileContentAfter[key]}`;
+      }
+    }
+    if (!_.has(fileContentBefore, key) && _.has(fileContentAfter, key)) {
+      result = `${result}\n + ${key}: ${fileContentAfter[key]}`;
+    }
+    if (_.has(fileContentBefore, key) && !_.has(fileContentAfter, key)) {
+      result = `${result}\n - ${key}: ${fileContentBefore[key]}`;
+    }
+  });
+  fs.closeSync(fdBefore);
+  fs.closeSync(fdAfter);
+  return `{\n${result}\n}`;
+};
 export default gendiff;
