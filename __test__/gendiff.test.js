fa53ca433ee32f7314745193a1e8e929bec61245
Evgeniy Sedov <sedoff-online@yandex.ru>
2019-07-12 00:28:57 +0700

    preparing configuration files and testing applications

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -4,13 +4,6 @@ const pathToBeforeYaml = './__test__/__fixtures__/yml/before.yml';
 const pathToAfterYaml = './__test__/__fixtures__/yml/after.yml';
 const pathToBeforeIni = './__test__/__fixtures__/ini/before.ini';
 const pathToAfterIni = './__test__/__fixtures__/ini/after.ini';
-const result = '{\n  host: hexlet.io\n- timeout: 50\n+ timeout: 20\n- proxy: 123.234.53.22\n- follow: false\n+ verbose: true\n}';
-test.each([[pathToBeforeYaml, pathToAfterYaml, result], [pathToBeforeIni, pathToAfterIni, result]])(
-  'gendiff(%s, %s)',
-  (pathBefore, pathAfter, expected) => {
-    expect(gendiff(pathBefore, pathAfter)).toBe(expected);
-  },
-);
 const pathToBeforeJson = './__test__/__fixtures__/json/before.json';
 const pathToAfterJson = './__test__/__fixtures__/json/after.json';
 const resultRecurs = `{
@@ -47,6 +40,7 @@ const resultRecurs = `{
       fee: 100500
   }
 }`;
-test('рекурсивное сравнение данных', () => {
-  expect(gendiff(pathToBeforeJson, pathToAfterJson)).toBe(resultRecurs);
+
+test.each([[pathToBeforeYaml, pathToAfterYaml, resultRecurs], [pathToBeforeIni, pathToAfterIni, resultRecurs], [pathToBeforeJson, pathToAfterJson, resultRecurs]])('gendiff(%s, %s)', (pathBefore, pathAfter, expected) => {
+  expect(gendiff(pathBefore, pathAfter)).toBe(expected);
 });
