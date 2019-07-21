0d33f7074c050f0e508e1acf1d742b95784ac9a6
Evgeniy Sedov <sedoff-online@yandex.ru>
2019-07-22 00:46:58 +0700

    fix src/formatters/index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,9 +2,4 @@ import stringify from './stringify';
 import plain from './plain';
 
 // eslint-disable-next-line import/prefer-default-export
-const formatter = (arg) => {
-  const currentFormater = (arg === 'plain') ? plain : stringify;
-  return currentFormater;
-};
-
-export default formatter;
+export default arg => ((arg === 'plain') ? plain : stringify);
