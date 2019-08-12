c02cbb1fa6cc8897437a417468bc320db6fc7ea5
Evgeniy Sedov <sedoff-online@yandex.ru>
2019-08-12 23:12:35 +0700

    step 6 finished

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,22 +1,31 @@
 // eslint-disable-next-line import/prefer-default-export
-const plain = (data) => {
+const plain = (data, keys = []) => {
   const result = data.reduce((acc, el) => {
+    let newStr;
+    let pathFromKeys;
     if (el instanceof Array) {
       const value0 = (el[0].value instanceof Object) ? '[complex value]' : el[0].value;
       const value1 = (el[1].value instanceof Object) ? '[complex value]' : el[1].value;
-      return [...acc, `Property '${el[0].key}' was updated. From ${value0} to ${value1}\n`];
+      keys.push(el[0].key);
+      pathFromKeys = keys.join('.');
+      keys.pop();
+      newStr = `Property '${pathFromKeys}' was updated. From ${value0} to ${value1}\n`;
+    } else {
+      const value = (el.value instanceof Object) ? '[complex value]' : el.value;
+      keys.push(el.key);
+      pathFromKeys = keys.join('.');
+      keys.pop();
+      if (el.value instanceof Array) {
+        newStr = `${plain(el.value, [...keys, el.key])}`;
+      } else if (el.sign === '+') {
+        newStr = `Property '${pathFromKeys}' was added with value: ${value}\n`;
+      } else if (el.sign === '-') {
+        newStr = `Property '${pathFromKeys}' was removed\n`;
+      } else {
+        newStr = '';
+      }
     }
-    const value = (el.value instanceof Object) ? '[complex value]' : el.value;
-    if (el.value instanceof Array) {
-      return [...acc, `${plain(el.value)}`];
-    }
-    if (el.sign === '+') {
-      return [...acc, `Property '${el.key}' was added with value: ${value}\n`];
-    }
-    if (el.sign === '-') {
-      return [...acc, `Property '${el.key}' was removed\n`];
-    }
-    return [...acc, ''];
+    return [...acc, newStr];
   }, []);
   const resultStr = `${result}`;
   return resultStr.replace(/,/g, '');
