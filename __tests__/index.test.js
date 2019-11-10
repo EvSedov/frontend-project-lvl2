8639ec66a5da6ec708ba4486d4ac95be4e1f0541
Evgeniy Sedov <sedoff-online@yandex.ru>
2019-11-10 23:25:25 +0700

    add test for json

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,22 +7,33 @@ const getFixturePath = name => path.join(__dirname, '..', '__fixtures__', name);
 
 let expectedStringify;
 let expectedPlain;
+let expectedJSON;
+let resultJSON;
 
 beforeAll(async () => {
-  expectedStringify = await fs.readFile(getFixturePath('stringify/resultStringify.txt'), 'utf-8');
-  expectedPlain = await fs.readFile(getFixturePath('plain/resultPlain.txt'), 'utf-8');
+  expectedStringify = await fs.readFile(getFixturePath('resultStringify.txt'), 'utf-8');
+  expectedPlain = await fs.readFile(getFixturePath('resultPlain.txt'), 'utf-8');
+  resultJSON = await fs.readFile(getFixturePath('resultJSON.json'), 'utf-8');
+  expectedJSON = await JSON.parse(resultJSON);
 });
 
 test.each(formats)('format stringify gendiff for %s', async (format) => {
-  const fileBeforePath = await getFixturePath(`stringify/before.${format}`);
-  const fileAfterPath = await getFixturePath(`stringify/after.${format}`);
+  const fileBeforePath = await getFixturePath(`before.${format}`);
+  const fileAfterPath = await getFixturePath(`after.${format}`);
   const actual = await gendiff(fileBeforePath, fileAfterPath, 'stringify');
   expect(actual).toBe(expectedStringify);
 });
 
 test.each(formats)('format plain gendiff for %s', async (format) => {
-  const fileBeforePath = getFixturePath(`plain/before.${format}`);
-  const fileAfterPath = getFixturePath(`plain/after.${format}`);
+  const fileBeforePath = getFixturePath(`before.${format}`);
+  const fileAfterPath = getFixturePath(`after.${format}`);
   const actual = await gendiff(fileBeforePath, fileAfterPath, 'plain');
   expect(actual).toBe(expectedPlain);
+});
+
+test.each(formats)('format json gendiff for %s', async (format) => {
+  const fileBeforePath = getFixturePath(`before.${format}`);
+  const fileAfterPath = getFixturePath(`after.${format}`);
+  const actual = await gendiff(fileBeforePath, fileAfterPath, 'json');
+  expect(actual).toStrictEqual(expectedJSON);
 });
