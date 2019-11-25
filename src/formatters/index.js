e5eacad4844443e117b7e19f46aa71950159fa99
Evgeniy Sedov <sedoff-online@yandex.ru>
2019-11-26 00:25:28 +0700

    fix src/formatters/index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,16 +2,6 @@ import stringify from './stringify';
 import plain from './plain';
 import json from './json';
 
-// eslint-disable-next-line import/prefer-default-export
-export default (arg) => {
-  switch (arg) {
-    case 'plain':
-      return plain;
-    case 'stringify':
-      return stringify;
-    case 'json':
-      return json;
-    default:
-      return plain;
-  }
-};
+const formates = { plain, stringify, json };
+
+export default arg => formates[arg];
