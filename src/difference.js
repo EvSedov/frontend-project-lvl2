41afad5f4133589399b1898c8f08bbac85231b4a
Evgeniy Sedov <sedoff-online@yandex.ru>
2019-12-01 15:27:55 +0700

    fix src/difference.js

--- src/difference.js
+++ src/difference.js
@@ -2,26 +2,10 @@ const getDifference = data => data.map((elem) => {
   const { sign, key } = elem;
   const value = (elem.type === 'nested') ? getDifference(elem.children) : elem.value;
   let result;
-  switch (elem.type) {
-    case 'added':
-      result = { sign, key, value };
-      break;
-    case 'deleted':
-      result = { sign, key, value };
-      break;
-    case 'changed':
-      result = [{ sign: sign[0], key, value: value[0] }, { sign: sign[1], key, value: value[1] }];
-      break;
-    case 'unchanged':
-      result = { sign, key, value };
-      break;
-    case 'nested':
-      result = { sign, key, value };
-      break;
-    default:
-      console.log(elem.type);
-      console.log('Unknown type.');
-      break;
+  if (elem.type === 'changed') {
+    result = [{ sign: sign[0], key, value: value[0] }, { sign: sign[1], key, value: value[1] }];
+  } else {
+    result = { sign, key, value };
   }
   return result;
 });
