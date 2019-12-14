f15f5480ecdc857321b5f8d20c0d4d531037b5db
Evgeniy Sedov <sedoff-online@yandex.ru>
2019-12-15 01:50:10 +0700

    fix stringify.js

--- src/formatters/stringify.js
+++ src/formatters/stringify.js
@@ -1,38 +1,51 @@
+import _ from 'lodash';
+
 const signs = {
   added: '+',
   deleted: '-',
   changed: ['-', '+'],
 };
-
 const getSign = (object, type) => (object[type] ? object[type] : ' ');
 
+const getValue = (tabBegin, tabEnd, value) => (
+  (_.isObject(value))
+    ? `{\n${' '.repeat(tabBegin)}${Object.keys(value).join()}: ${Object.values(value).join()}\n${' '.repeat(tabEnd)}}`
+    : value
+);
+
+const collectorStrings = {
+  changed: args => [
+    `\n${' '.repeat(args[0])}${args[1][0]} ${args[2]}: ${args[3][0]}`,
+    `\n${' '.repeat(args[0])}${args[1][1]} ${args[2]}: ${args[3][1]}`,
+  ],
+  nested: args => (
+    `\n${' '.repeat(args[0])}${args[1]} ${args[2]}: ${args[3](args[4], args[0] + 2)}`
+  ),
+};
+const getString = args => `\n${' '.repeat(args[0])}${args[1]} ${args[2]}: ${args[3]}`;
+const getCollectorString = (object, type) => (object[type] ? object[type] : getString);
+
 const stringify = (data, numberOfSpaces = 0) => {
-  const currentNumberOfSpaces = numberOfSpaces;
-  const spasesAtInBeginning = ' '.repeat(currentNumberOfSpaces + 8);
-  const spasesAtEnd = ' '.repeat(currentNumberOfSpaces + 4);
+  const currentNumberOfSpaces = numberOfSpaces + 2;
+  const spacesAtInBeginning = currentNumberOfSpaces + 6;
+  const spacesAtEnd = currentNumberOfSpaces + 2;
   const result = data.map((elem) => {
     const {
       type, key, value,
     } = elem;
-    if (type === 'changed') {
-      const strValue0 = (value[0] instanceof Object)
-        ? `{\n${spasesAtInBeginning}${Object.keys(value[0]).join()}: ${Object.values(value[0]).join()}\n${spasesAtEnd}}`
-        : value[0];
-      const strValue1 = (value[1] instanceof Object)
-        ? `{\n${spasesAtInBeginning}${Object.keys(value[1]).join()}: ${Object.values(value[1]).join()}\n${spasesAtEnd}}`
-        : value[1];
-      return [`\n  ${' '.repeat(currentNumberOfSpaces)}${getSign(signs, type)[0]} ${key}: ${strValue0}`,
-        `\n  ${' '.repeat(currentNumberOfSpaces)}${getSign(signs, type)[1]} ${key}: ${strValue1}`];
-    }
-    const strValue = (value instanceof Object)
-      ? `{\n${spasesAtInBeginning}${Object.keys(value).join()}: ${Object.values(value).join()}\n${spasesAtEnd}}`
-      : value;
-    if (type === 'nested') {
-      return `\n  ${' '.repeat(currentNumberOfSpaces)}  ${key}: ${stringify(elem.children, numberOfSpaces + 4)}`;
-    }
-    return `\n  ${' '.repeat(currentNumberOfSpaces)}${getSign(signs, type)} ${key}: ${strValue}`;
+    const sign = getSign(signs, type);
+    const strValue = (type === 'changed')
+      ? [
+        getValue(spacesAtInBeginning, spacesAtEnd, value[0]),
+        getValue(spacesAtInBeginning, spacesAtEnd, value[1]),
+      ]
+      : getValue(spacesAtInBeginning, spacesAtEnd, value);
+    const args = (type === 'nested')
+      ? [currentNumberOfSpaces, sign, key, stringify, elem.children]
+      : [currentNumberOfSpaces, sign, key, strValue];
+    return getCollectorString(collectorStrings, type)(args);
   });
-  const resultStr = `{${result}\n${' '.repeat(currentNumberOfSpaces)}}`;
+  const resultStr = `{${result}\n${' '.repeat(currentNumberOfSpaces - 2)}}`;
   return resultStr.replace(/,/g, '');
 };
 
