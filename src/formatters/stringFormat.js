fa0a3916f183145fa59bea43ff6d9a397b75847b
Evgeniy Sedov <sedoff-online@yandex.ru>
2020-01-31 00:38:10 +0700

    fix stringify.js logic through types

--- src/formatters/stringFormat.js
+++ src/formatters/stringFormat.js
@@ -1,12 +1,5 @@
 import _ from 'lodash';
 
-const setOfSigns = {
-  added: '+',
-  deleted: '-',
-  changed: ['-', '+'],
-};
-const getSign = (object, type) => (object[type] ? object[type] : ' ');
-
 const getSpace = (num) => ' '.repeat(num * 4);
 
 const getValue = (value, depth) => {
@@ -18,29 +11,28 @@ const getValue = (value, depth) => {
 };
 
 const parseForType = {
-  changed: (signs, key, values, depth) => [
-    `\n${getSpace(depth)}${signs[0]} ${key}: ${getValue(values[0], depth)}`,
-    `\n${getSpace(depth)}${signs[1]} ${key}: ${getValue(values[1], depth)}`,
+  added: ({ key, value }, depth) => `\n${getSpace(depth)}+ ${key}: ${getValue(value, depth)}`,
+
+  changed: ({ key, value }, depth) => [
+    `\n${getSpace(depth)}- ${key}: ${getValue(value[0], depth)}`,
+    `\n${getSpace(depth)}+ ${key}: ${getValue(value[1], depth)}`,
   ].join(''),
 
-  nested: (sign, key, value, depth) => (
+  deleted: ({ key, value }, depth) => `\n${getSpace(depth)}- ${key}: ${getValue(value, depth)}`,
+
+  nested: ({ key, children }, depth) => {
     // eslint-disable-next-line no-use-before-define
-    `\n${getSpace(depth)}${sign} ${key}: {${value.map((el) => stringify(el, depth + 1))}
-  ${getSpace((depth))}}`
-  ),
+    const innerText = children.map((el) => stringify(el, depth + 1));
+    return `\n${getSpace(depth)}  ${key}: {${innerText}
+  ${getSpace(depth)}}`;
+  },
+
+  unchanged: ({ key, value }, depth) => `\n${getSpace(depth)}  ${key}: ${getValue(value, depth)}`,
 };
 
-const parseForOthersType = (sign, key, value, depth) => `\n${getSpace(depth)}${sign} ${key}: ${getValue(value, depth)}`;
-
-const getFuncForBuildString = (object, type) => (
-  (object[type]) ? object[type] : parseForOthersType
-);
-
 const stringify = (element, depth = 1) => {
-  const { type, key, value } = element;
-  const currentValue = (element.children) ? element.children : value;
-  const sign = getSign(setOfSigns, type);
-  return getFuncForBuildString(parseForType, type)(sign, key, currentValue, depth);
+  const { type } = element;
+  return parseForType[type](element, depth);
 };
 
 export default (data) => `{${data.map((el) => stringify(el, 1))}\n}`.split(',').join('');
